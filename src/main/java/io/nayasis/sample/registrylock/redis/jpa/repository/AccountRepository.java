b85ce96541a1234e7f14d26019fda6cbf304319a
1000084 <nayasis@onestorecorp.com>
2021-02-24 18:04:22 +0900

    delete previous source

--- src/main/java/io/nayasis/sample/registrylock/redis/jpa/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package io.nayasis.sample.registrylock.redis.jpa.repository;
-
-import io.nayasis.sample.registrylock.redis.jpa.entity.AccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<AccountEntity,Long> {
-}
